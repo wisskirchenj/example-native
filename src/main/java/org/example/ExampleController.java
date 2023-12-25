0172b91adad4dc49cf368b3486cd49f3c4efe606
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-12-26 00:12:54 +0100

    Create demo repo for spring boot issue

--- src/main/java/org/example/ExampleController.java
+++ src/main/java/org/example/ExampleController.java
@@ -3,14 +3,6 @@ package org.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * RESTController-class offering the following endpoints:
- * GET seats - JSON object of a cinema room displaying all available seats with prices
- * POST purchase - receiving a seat key in the request body for which a ticket is purchased, if
- *                 seat is valid and  available -> also returns a token for possible refunding
- * POST return - receiving a token in request body and refunds the seat
- * Post stats - returns the current SalesStatistic to a password authorized requester
- */
 @RestController
 public class ExampleController {
 
